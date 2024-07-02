18496e90b759f251703fd075fa460765a7a5c070
baphometapopis <zxyz9357@gmail.com>
2024-07-02 16:27:33 +0530

    changed encryption

--- src/Utils/encryption.js
+++ src/Utils/encryption.js
@@ -4,11 +4,14 @@ const secretKey = 'Ezclick-2024';  // You should store this securely
 
 // Encrypt function
 export const encrypt = (data) => {
-  return CryptoJS.AES.encrypt(data, secretKey).toString();
+  const encryptedData = CryptoJS.AES.encrypt(data, secretKey).toString();
+  const base64EncryptedData = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encryptedData));
+  return base64EncryptedData;
 };
 
 // Decrypt function
 export const decrypt = (ciphertext) => {
-  const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+  const parsedEncryptedData = CryptoJS.enc.Base64.parse(ciphertext).toString(CryptoJS.enc.Utf8);
+  const bytes = CryptoJS.AES.decrypt(parsedEncryptedData, secretKey);
   return bytes.toString(CryptoJS.enc.Utf8);
 };
